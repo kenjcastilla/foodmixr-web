24688b3ad1e1d3d383b292c6063ff8746ce3fe68
Kendall Castilla <kjc249@hotmail.com>
2023-09-12 21:07:29 -0400

    setCurrentTrack now only refreshes after duration of current track is reached.

--- src/spotify/SetCurrentTrack.js
+++ src/spotify/SetCurrentTrack.js
@@ -1,26 +1,60 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
-import { db, firebaseApp } from "../firebase/config";
-import { doc, getDoc, updateDoc } from "firebase/firestore";
-import { useAuth } from "../context/AuthContext";
+import { db } from "../firebase/config";
+import { doc, updateDoc } from "firebase/firestore";
 import axios from "axios";
 import { SPOTIFY_LOGIN } from "../constants/routes";
 
 const CURRENT_TRACK_ENDPOINT = "https://api.spotify.com/v1/me/player/currently-playing";
 
 const SetCurrentTrack = () => {
+   const [duration, setDuration] = useState(240000);
+   const [trackTitle, setTrackTitle] = useState("");
    const navigate = useNavigate();
    const localUser = JSON.parse(localStorage.getItem("localUser"));
    const uid = localUser.uid;
-   const code = JSON.stringify(localStorage.getItem("localUser")).code;
 
-   const helpSetToken = () => {
-      const tokenInStorage = JSON.parse(JSON.stringify(localStorage.getItem("spotifyToken")));
-      console.log(tokenInStorage);
-      return tokenInStorage;
-   };
+   const getTime = useCallback(() => {
+      console.log(`DURATION:  ${duration}`);
+      return duration;
+   }, [duration]);
 
-   const getCurrentTrack = () => {
+   const setTrackInFirestore = useCallback(async (data) => {
+      const album = data.item.album.name;
+      const image = data.item.album.images[0].url;
+      const timeLeft = data.item.duration_ms - data.progress_ms;
+      const title = data.item.name;
+      if (title !== trackTitle) {
+         setTrackTitle(title);
+         setDuration(timeLeft);
+         const trackLink = data.item.external_urls.spotify;
+         const artists = data.item.artists.reduce((arr, item) => {
+            arr.push(item.name);
+            return arr;
+         }, []);
+         console.log(artists);
+         console.log("setTrackInFirestore initiated...")
+         const toUpdate = {
+            album: album,
+            artists: artists,
+            image: image,
+            name: title,
+            link: trackLink,
+            time_left: timeLeft,
+         };
+         const docRef = doc(db, "current-track", uid);
+         await updateDoc(docRef, toUpdate)
+            .then(docRef => {
+               console.log("Current track successfully updated in Firestore.");
+               return timeLeft;
+            })
+            .catch(error => { console.log(error); })
+      }
+      // return timeLeft;
+
+   }, [trackTitle, uid]);
+
+   const getCurrentTrack = useCallback(() => {
       const token = JSON.parse(JSON.stringify(localStorage.getItem("spotifyToken")));
       axios.get(CURRENT_TRACK_ENDPOINT, {
          headers: {
@@ -28,7 +62,6 @@ const SetCurrentTrack = () => {
          },
       })
          .then((response) => {
-            console.log(`Get Current Track response: ${response.data}`);
             setTrackInFirestore(response.data);
          })
          .catch((error) => {
@@ -36,53 +69,23 @@ const SetCurrentTrack = () => {
             console.log('TOKEN EXPIRED. REDIRECTING TO SPOTIFY LOGIN PAGE...');
             navigate(SPOTIFY_LOGIN);
          });
-   };
+   }, [navigate, setTrackInFirestore]);
 
-   const setTrackInFirestore = async (data) => {
-      const album = data.item.album.name;
-      const image = data.item.album.images[0].url;
-      const timeLeft = data.item.duration_ms - data.progress_ms;
-      const title = data.item.name;
-      const trackLink = data.item.external_urls.spotify;
-      const artists = data.item.artists.reduce((arr, item) => {
-         arr.push(item.name);
-         return arr;
-      }, []);
-      console.log(artists);
-      // localStorage.setItem("title", title);
-      // localStorage.setItem("artists", artists);
-      console.log("setTrackInFirestore initiated...")
-      const toUpdate = {
-         album: album,
-         artists: artists,
-         image: image,
-         name: title,
-         link: trackLink,
-         time_left: timeLeft,
-      };
-      const docRef = doc(db, "current-track", uid);
-      await updateDoc(docRef, toUpdate)
-         .then(docRef => {
-            console.log("Current track successfully updated in Firestore.");
-            return timeLeft;
-         })
-         .catch(error => { console.log(error); })
-      // return timeLeft;
-
-   };
 
    useEffect(() => {
+      console.log(`DURATION: ${duration}`);
       getCurrentTrack();
-      setInterval(() => {
+      setTimeout(() => {
          window.location.reload(false);
-      }, 90000);
+      }, duration);
 
-   });
+   }, [duration, getCurrentTrack]);
+   
    return (
       <>
       <h1 style={{ width: '75%', margin: 'auto', marginTop: '6em', textAlign: 'center', fontSize: 40 }}>
          You are currently sharing your playback with others!
-         <br />Leaving this page will cease updating your viewers on what you're listening to.
+         <br />Keep this page open to continue sharing.
       </h1>
       </>
 
